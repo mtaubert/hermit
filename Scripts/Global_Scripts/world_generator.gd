3ba7249d39048992abfa25bdf1f6a2ce1057bc42
Max <m.taubert2015@gmail.com>
2019-04-10 15:17:20 +0100

    Added world generation

--- Scripts/Global_Scripts/world_generator.gd
+++ Scripts/Global_Scripts/world_generator.gd
@@ -1,7 +1,22 @@
 extends Node
 
 func _ready():
-	pass # Replace with function body.
+	randomize()
 
-func generate_world(size:Vector2):
-	pass
+var depthNoise
+var biomeNoise
+var size
+
+func generate_world(octaves, period, persistence, worldSize:Vector2):
+	depthNoise = OpenSimplexNoise.new()
+	size = worldSize
+	# Configure
+	depthNoise.seed = randi()
+	depthNoise.octaves = octaves
+	depthNoise.period = period
+	depthNoise.persistence = persistence
+
+func get_depth(x,y):
+	var gradientOffset = (((((x/size.x)+(y/size.y))/2)*2)-1)*(-1)
+	var depth = (gradientOffset+depthNoise.get_noise_2d(x,y))/2
+	return depth
