0106165980310696e70fc430988b8642d468dd8f
Bram <bramreth@gmail.com>
2019-04-10 16:01:09 +0100

    fullblown attacking with parameters and a cooldown and jazz

--- Scripts/Global_Scripts/player_crab.gd
+++ Scripts/Global_Scripts/player_crab.gd
@@ -4,14 +4,21 @@ var velocity := Vector2(0,0)
 var direction := Vector2(0,0)
 export (int) var MAX_SPEED = 500
 export (int) var ATTACKING_SPEED = 200
+export (int) var ATTACK_RANGE = 200
+#how long it lasts. shorter is better
+export (float) var ATTACK_SPEED = 0.5
+export (float) var ATTACK_COOLDOWN = 0.2
 var attacking = false
+var lock_input = false
+var can_attack = true
 
 func _ready():
-	pass # Replace with function body.
+	$attack_cooldown.wait_time = ATTACK_COOLDOWN + ATTACK_SPEED
 
 func _process(delta):
-	velocity = handle_movement()
-	$KinematicBody2D.move_and_slide(velocity)
+	if not lock_input:
+		velocity = handle_movement()
+		$KinematicBody2D.move_and_slide(velocity)
 
 """
 assess user inputs to get movement direction, returns what the players velocity should be from user input
@@ -49,11 +56,35 @@ func update_look_direction():
 func look_to(vect):
 	$KinematicBody2D/Node2D.rotation_degrees = rad2deg(vect.angle()) + 90
 
+#takes a normalised vector as the attack direction
+func attack(target):
+	$attack_cooldown.start()
+	can_attack = false
+	lock_input = true
+	$attack_tween.interpolate_property(self, "position", self.position, self.position + target * ATTACK_RANGE,
+	 ATTACK_SPEED, Tween.TRANS_BACK, Tween.EASE_IN)
+	$attack_tween.start()
+	
 func _input(event):
 # Mouse in viewport coordinates
-	if event is InputEventMouseButton:
-		attacking = not attacking
-		print("Mouse Click/Unclick at: ", get_global_mouse_position())
-		look_to(get_global_mouse_position()-$KinematicBody2D.global_position)
-	if event is InputEventMouseMotion and attacking:
-		look_to(get_global_mouse_position()-$KinematicBody2D.global_position)
+	if not lock_input:
+		var target = (get_global_mouse_position()-$KinematicBody2D.global_position).normalized()
+		if event is InputEventMouseButton:
+			if event.button_index == BUTTON_LEFT:
+				if event.pressed:
+					attacking = true
+					look_to(target)
+				elif can_attack:
+					attacking = false
+					look_to(target)
+					attack(target)
+		if attacking:
+			look_to(target)
+
+#func _on_attack_tween_tween_completed(object, key):
+#	lock_input = false
+
+
+func _on_attack_cooldown_timeout():
+	can_attack = true
+	lock_input = false
