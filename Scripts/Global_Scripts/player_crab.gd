d166b92abd19dfb0e67f22181f6bbb0e4d6689ce
Bram <bramreth@gmail.com>
2019-04-10 15:22:24 +0100

    attacking at mouse position

--- Scripts/Global_Scripts/player_crab.gd
+++ Scripts/Global_Scripts/player_crab.gd
@@ -3,6 +3,8 @@ extends Node2D
 var velocity := Vector2(0,0)
 var direction := Vector2(0,0)
 export (int) var MAX_SPEED = 500
+export (int) var ATTACKING_SPEED = 200
+var attacking = false
 
 func _ready():
 	pass # Replace with function body.
@@ -18,8 +20,12 @@ func handle_movement():
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	direction = direction.normalized()
+	
 	update_look_direction()
-	return (direction.normalized() * MAX_SPEED)
+	if attacking:
+		return (direction.normalized() * ATTACKING_SPEED)
+	else:
+		return (direction.normalized() * MAX_SPEED)
 	
 	
 """
@@ -28,8 +34,8 @@ we want the crab by default to look in the direction its moving, on click for an
 func update_look_direction():
 	#oly update the look direction if there is input
 	if velocity.x or velocity.y:
-		#add a magical 90 degree offset for the rotation of the crab
-		$KinematicBody2D/Node2D.rotation_degrees = rad2deg(velocity.angle()) + 90
+		if not attacking:
+			look_to(velocity)
 		
 		#set the crab to scuttle if moving
 		if not $KinematicBody2D/Node2D/AnimationPlayer.is_playing():
@@ -37,3 +43,17 @@ func update_look_direction():
 	else:
 		#stop scuttling if stationary
 		$KinematicBody2D/Node2D/AnimationPlayer.stop()
+
+
+#add a magical 90 degree offset for the rotation of the crab
+func look_to(vect):
+	$KinematicBody2D/Node2D.rotation_degrees = rad2deg(vect.angle()) + 90
+
+func _input(event):
+# Mouse in viewport coordinates
+	if event is InputEventMouseButton:
+		attacking = not attacking
+		print("Mouse Click/Unclick at: ", get_global_mouse_position())
+		look_to(get_global_mouse_position()-$KinematicBody2D.global_position)
+	if event is InputEventMouseMotion and attacking:
+		look_to(get_global_mouse_position()-$KinematicBody2D.global_position)
